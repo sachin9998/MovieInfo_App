82e64737749543edffd80cc6cff780275f0e4191
Sachin Alam <sachinalam4216@gmail.com>
2024-08-15 17:44:51 +0530

    Fixed Search issue

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -7,7 +7,7 @@ import Loader from "../components/Loader";
 const SearchPage = () => {
   const location = useLocation();
   const [searchData, setSearchData] = useState([]);
-  const [page, setPage] = useState(1)
+  const [page, setPage] = useState(1);
   const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
 
@@ -21,12 +21,9 @@ const SearchPage = () => {
       setSearchData((prev) => {
         return [...prev, ...response.data.results];
       });
-
     } catch (error) {
       console.log(error);
-    }
-
-    finally {
+    } finally {
       setLoading(false);
     }
   };
@@ -34,15 +31,12 @@ const SearchPage = () => {
   const query = location?.search?.slice(3);
 
   useEffect(() => {
-
     if (query) {
       setPage(1);
       setSearchData([]);
       fetchData();
     }
-
-
-  }, [location?.search])
+  }, [location?.search]);
 
   const handleScroll = () => {
     if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
@@ -60,47 +54,48 @@ const SearchPage = () => {
     window.addEventListener("scroll", handleScroll);
   }, []);
 
-  if (loading) {
-    <>
-      <Loader />
-    </>
-  }
-
-  else return (
+  return (
     <div className="py-16">
-
       {/* Mobile version Search Box */}
       <div className="lg:hidden my-2 mx-1 sticky top-[70px] z-50">
-
-        <input type="text" placeholder="Search here..." onChange={(e) => navigate(`/search?q=${e.target.value}`)}
+        <input
+          type="text"
+          placeholder="Search here..."
+          onChange={(e) => navigate(`/search?q=${e.target.value}`)}
           value={query?.split("%20")?.join(" ")}
           className="px-4 py-1 text-lg w-full bg-white rounded-full text-neutral-900"
         />
-
-
       </div>
 
       <div className="container mx-auto ">
+        <h3 className="capitalize text-lg lg:text-xl font-semibold my-3">
+          Search Results
+        </h3>
 
-        <h3 className="capitalize text-lg lg:text-xl font-semibold my-3">Search Results</h3>
-
-        <div className="grid grid-cols-[repeat(auto-fit,230px)] gap-6 justify-center lg:justify-start">
-          {searchData.map((data, index) => {
-            {
-              /* { exploreData.id + "exploreSection" } */
-            }
-            return (
-              <Card
-                data={data}
-                key={data.id + "SearchSection" + index}
-                media_type={data.media_type}
-              />
-            );
-          })}
-        </div>
-
+        {loading ? (
+          <>
+            {" "}
+            <Loader />{" "}
+          </>
+        ) : (
+          <div className="grid grid-cols-[repeat(auto-fit,230px)] gap-6 justify-center lg:justify-start">
+            {searchData.map((data, index) => {
+              {
+                /* { exploreData.id + "exploreSection" } */
+              }
+              return (
+                <Card
+                  data={data}
+                  key={data.id + "SearchSection" + index}
+                  media_type={data.media_type}
+                />
+              );
+            })}
+          </div>
+        )}
       </div>
-    </div>);
+    </div>
+  );
 };
 
 export default SearchPage;
