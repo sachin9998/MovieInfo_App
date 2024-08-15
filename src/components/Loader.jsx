6cbfb9ca0f3f2eae5648920b4529d5c992ad8885
Sachin Alam <sachinalam4216@gmail.com>
2024-08-15 11:22:17 +0530

    New Laoder added

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,20 +1,9 @@
-import { InfinitySpin } from "react-loader-spinner"
-
 const Loader = () => {
     return (
         <div className="h-[93vh] lg:h-[85vh] flex justify-center items-center">
-            {/* <div className="progress pulse">
-            </div> */}
-
-            <InfinitySpin
-                visible={true}
-                width="200"
-                color="#ff9007"
-                ariaLabel="infinity-spin-loading"
-            />
-
+            <div className="progress pulse"></div>
         </div>
-    )
-}
+    );
+};
 
-export default Loader
+export default Loader;
